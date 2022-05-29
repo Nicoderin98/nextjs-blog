200d97aba7fc4fa36be71744d1fa1257d9216d35
Nicolo De Rinaldis <nicoderin98@gmail.com>
2022-05-29 20:07:25 +0200

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="text-center text-white fixed-bottom" >
+  <div className="container p-4"></div>
+
+  <div className="text-center p-3">
+    © 2022 Copyright:
+    <a className="text-white" href=" https://github.com/Nicoderin98"> Nicolò De Rinaldis</a>
+  </div>
+</footer>
+  );
+}
+export default Footer;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./pages/index.tsx}",
+    "./components/Navbar.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
